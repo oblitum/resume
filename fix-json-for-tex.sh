df01b030b6cacde40a844eceeb4a91f363bbe1e5
Francisco Lopes <francisco@oblita.com>
2014-07-01 05:14:16 -0300

    Adds generated resume web page

--- fix-json-for-tex.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cp resume.json resume-for-tex.json
-sed -i '' 's/C#/C\\\\#/' resume-for-tex.json
-sed -i '' 's/\&amp;/\\\\\&/' resume-for-tex.json
-sed -i '' 's/669#/669\\\\#/' resume-for-tex.json
