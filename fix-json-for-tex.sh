e4b6f191a09626b6d97c77dadcf6c7733d395c06
Francisco Lopes <francisco@oblita.com>
2014-07-02 17:25:12 -0300

    Fix tilde escaping

--- fix-json-for-tex.sh
+++ fix-json-for-tex.sh
@@ -4,3 +4,4 @@ cp resume.json resume-for-tex.json
 sed -i '' 's/C#/C\\\\#/' resume-for-tex.json
 sed -i '' 's/\&amp;/\\\\\&/' resume-for-tex.json
 sed -i '' 's/669#/669\\\\#/' resume-for-tex.json
+sed -i '' 's/~/$\\\\sim$/' resume-for-tex.json
